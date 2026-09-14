0234acce00bb0af1562f0e2eb0bf40755372ec58
Surya-K-L <suryakannaiyan4@gmail.com>
2026-09-14 18:46:29 +0530

    Task 4 Completed

---
+++ Day4/PracticeTask/Task4.py
@@ -0,0 +1,8 @@
+import numpy as np
+numbers=np.arange(1,13)
+num=numbers.reshape(3,4)
+print("\nReshape: ",num)
+print("\nDimension: ",num.ndim)
+print("\nShape: ",num.shape)
+print("\nSize: ",num.size)
+
