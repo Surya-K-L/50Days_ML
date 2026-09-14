4c0a35216a164908b8272ea39e22e8293ee0ec74
Surya-K-L <suryakannaiyan4@gmail.com>
2026-09-14 18:09:02 +0530

    Task1

---
+++ Day4/PracticeTask/Task1.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("First Element: ",arr[0])
+print("Last Element: ",arr[arr.size-1])
+print("Number of Elements: ",arr.size)
+print("Minimum: ",np.min(arr))
+print("Maximum: ",np.max(arr))
+print("Mean: ",np.mean(arr))
