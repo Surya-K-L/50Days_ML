86c8f8554b7413e7f2442c5a05068fa375377c99
Surya-K-L <suryakannaiyan4@gmail.com>
2026-09-14 18:40:42 +0530

    Task 3 Completed

---
+++ Day4/PracticeTask/Task3.py
@@ -0,0 +1,6 @@
+import numpy as np
+numbers=np.array([10, 20, 30, 40, 50, 60, 70, 80])
+print("\nFrist Four Values: ",numbers[:4])
+print("\nLast 3 values: ",numbers[-3:])
+print("\nPrint 20 to 60: ",numbers[1:6])
+print("\nEvery Second Value: ",numbers[1::2])
