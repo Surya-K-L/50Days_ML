37603e4579932b56cdad516d7a51611350a6e104
Surya-K-L <suryakannaiyan4@gmail.com>
2026-09-14 18:51:31 +0530

    Task 5 Completed

---
+++ Day4/PracticeTask/Task5.py
@@ -0,0 +1,8 @@
+import numpy as np
+marks= np.array([45, 78, 92, 56, 88, 72, 95, 61, 83, 77])
+print("\nMean: ",np.mean(marks))
+print("\nMedian: ",np.median(marks))
+print("\nStandard Deviation: ",np.std(marks))
+print("\nMinimum Mark: ",np.min(marks))
+print("\nMaximum Mark: ",np.max(marks))
+print("\nSum of Marks: ",np.sum(marks))
